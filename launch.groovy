f673785ab48ff78e779299a9634ebd61ccd56d5c
Kevin Harrington <mad.hephaestus@gmail.com>
2019-12-29 16:21:11 -0500

    new Robot content

---
+++ launch.groovy
@@ -0,0 +1,4 @@
+ScriptingEngine.gitScriptRun(	"https://github.com/OperationSmallKat/SmallKat_V2.git", 
+								"loadRobot.groovy", 
+["https://github.com/madhephaestus/Luna_copy.git",
+		"Luna_copy.xml","GameController_22","hidDevice","Luna_copy"]);
